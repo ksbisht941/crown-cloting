a8e7244ceb1273196f95fab20bb2df4cf53554a4
Kevin <kevin@Kevins-MacBook-Air.local>
2021-08-14 10:16:03 +0530

    create first react app

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div classname='homepage'>
+        <div classname='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 class='title'>WOMENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage
