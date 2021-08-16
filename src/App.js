9f2ff624d65c9afa9649357c78342b7ce18e9e5b
Kevin <kevin@Kevins-MacBook-Air.local>
2021-08-17 02:24:44 +0530

    header, shop and home done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  // BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
