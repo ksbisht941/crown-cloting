5f01561031ecb1eb98449f0032bb8b9c9e9bd8a8
Kevin <kevin@Kevins-MacBook-Air.local>
2021-08-17 15:44:02 +0530

    react form, custom input and button, signin page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component.jsx';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
